34779d3f15f86248615b829daf8803febab630c8
CTAPCKPIM <novickdanya@gmail.com>
2022-04-26 20:55:31 +0300

    Completed all items from task 3

--- HouseRegistry.sol
+++ HouseRegistry.sol
@@ -4,13 +4,20 @@ pragma solidity 0.8.0;
 
 ///@author by CTAPCKPIM
 ///@title Registry of houses
-contract HouseRegistry {
+
+import "./@openzeppelin/contracts/access/Ownable.sol";
+
+contract HouseRegistry is Ownable{
 
     ///@dev to display in an outer functions
     event NewHouse(uint256 id, address seller, uint256 cost, string addrHouse);
 
-    ///@dev variable for helping the generation ID
+    ///@dev the variable for helping the generation ID
     uint256 internal idNumber = 99999;
+    ///@dev the setting time of pause
+    uint256 internal readiness = 1 days;
+    ///@dev the variable for setting of time
+    uint256 internal timer;
 
     ///@dev struct for save of new houses
     struct House {
@@ -19,25 +26,27 @@ contract HouseRegistry {
         uint256 squareHouse;
         address seller;
         string addressHouse;
+        bool exist;
     }
 
     ///@dev mapping to save all new houses
     mapping (uint256 => House) public houses;
-    ///@dev mapping to preserves exist the ID of houses
-    mapping (uint256 => bool) private existId; 
 
     ///@param 'randId' creates a new ID for the house 
     ///@dev function for append new houses
     ///@dev return ID 
-    function listHouse(uint256 _costHouse, uint256 _squareHouse, address _seller, string memory _addressHouse) public returns(uint256) {
+    function _listHouse(uint256 _costHouse, uint256 _squareHouse, address _seller, string memory _addressHouse) internal returns(uint256) {
         require(_costHouse > 0, "ERROR: Price not can bee '0'");
         require(_squareHouse > 0, "ERROR: Square can not be '0'");
         uint256 randId = uint256(keccak256(abi.encodePacked(_seller, _squareHouse, _addressHouse))) % idNumber;
-        require(existId[randId] == false, "ERROR: This ID already exists");
-        existId[randId] = true;
-        houses[randId] = House(randId, _costHouse, _squareHouse, _seller, _addressHouse);
+        require(houses[randId].seller == address(0), "ERROR: This ID already exists");
+        houses[randId] = House(randId, _costHouse, _squareHouse, _seller, _addressHouse, true);
         emit NewHouse(randId, _seller, _costHouse, _addressHouse);
         return randId;
     } 
+    
+    ///@dev setting not activity house in register
+    function delistHouse(uint256 _id) public onlyOwner {
+        houses[_id].exist = false;
+    }
 }
-
--- HouseRegistryExt.sol
+++ HouseRegistryExt.sol
@@ -7,10 +7,22 @@ import "./HousesRegistry.sol";
 ///@author by CTAPCKPIM
 ///@title Simple registry of houses
 contract HouseRegistryExt is HouseRegistry {
+    
+    ///@dev the setting value of time
+    function _timeSum() internal {
+        timer = block.timestamp + readiness;
+    }
+
+    ///@dev return true or false 
+    function _timeCheck() internal view returns(bool) {
+        return timer <= block.timestamp;
+    }
 
     ///@dev this function is a simple registry of houses
     function listHouseSimple(uint256 _costHouse, uint256 _squareHouse, string memory _addressHouse) public {
         address _seller = msg.sender;
-        listHouse(_costHouse, _squareHouse, _seller, _addressHouse);
+        require(_timeCheck(), "ERROR: Adding a house will be available in 24 hours");
+        _listHouse(_costHouse, _squareHouse, _seller, _addressHouse);
+        _timeSum();
     }
 }
